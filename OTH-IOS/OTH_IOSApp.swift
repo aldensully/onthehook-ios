b6ced1e58b061877b2be34abaae6ed625aa692a0
aldensully <aw.sullivan17@gmail.com>
2023-11-20 15:44:37 -0500

    Initial Commit

---
+++ OTH-IOS/OTH_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTH_IOSApp.swift
+//  OTH-IOS
+//
+//  Created by Alden  Sullivan on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct OTH_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
